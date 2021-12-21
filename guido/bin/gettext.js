2018dbf9b5703712a570bc2099a8baff4b7f8a38
Assen Totin <assen.totin@gmail.com>
2021-12-21 23:57:20 +0200

    Update docs, new gettext collector

--- guido/bin/gettext.js
+++ guido/bin/gettext.js
@@ -21,7 +21,6 @@
  */
 
 var fs = require('fs');
-var LineReader = require('liner');
 
 // HTML-cleaning regular expression
 var reHtml = /<(?:.|\n)*?>/gm;
@@ -32,6 +31,7 @@ var reJsSplit = /;|\+/g;
 var reJsMatch = /'|"/;
 var reJsStrip1 = /^(.*_\(')(.*)('\).*)$/;
 var reJsStrip2 = /^(.*_\(")(.*)("\).*)$/;
+var reJsPath = /(\.\.\/)+(app\/)/;
 
 // PO-parsing regular expressions
 var rePoMsgid = /^(msgid.*")(.*)(".*)$/;
@@ -40,67 +40,54 @@ var rePoMsgstr = /^(msgstr.*")(.*)(".*)$/;
 // Our index file name
 var index_file = 'guido.index';
 
-// Root directory to traverse
-var base_dir = '';
-
 // File list array
 var list = [];
 
 // Array that will store all strings
 var stringsRaw = [];
 
-// Sync object to create the POT file once all files have been read
-var sync = new guidoSync(function() {
-	// Deduplicate the strings
-	var stringsDedup = deduplicate(stringsRaw);
-
-	// Generate the POT file
-	base_dir = '../../app/locale';
-	var pot = generatePot(stringsDedup);
-	fs.writeFileSync(base_dir + '/master.pot', pot);
-
-	// Report
-	console.log("* Generated new master POT file in " + base_dir + '/master.pot');
-
-
-	// Load existing PO files and merge the changes ito them
-	base_dir = '../../app/locale';
-	list = [];
-	walk(base_dir, list, null, null);
-	for (var i=0; i<list.length; i++)
-		if (list[i] != 'master.pot')
-			poProcessor(base_dir, list[i], stringsDedup);
-});
-
 // Walk APP templates
-base_dir = '../../app/templates';
-list = [];
-walk(base_dir, list, null, null);
-for (var i=0; i<list.length; i++) {
-	sync.inc();
-	lineProcessor(base_dir, list[i], 'html', stringsRaw);
-}
+walk('../../app/templates', 'html', list);
 
 // Walk APP JS
-base_dir = '../../app/js';
-list = [];
-walk(base_dir, list, null, null);
-for (var i=0; i<list.length; i++) {
-	sync.inc();
-	lineProcessor(base_dir, list[i], 'js', stringsRaw);
+walk('../../app/js', 'js', list);
+
+// Process files one by one
+for (var i=0; i < list.length; i++) 
+	lineProcessor(list[i], stringsRaw);
+
+// Deduplicate the strings
+var stringsDedup = deduplicate(stringsRaw);
+
+// Generate the POT file
+var base_dir = '../../app/locale';
+var pot = generatePot(stringsDedup);
+fs.writeFileSync(base_dir + '/master.pot', pot);
+
+// Report
+console.log("* Generated new master POT file in " + base_dir + '/master.pot');
+
+// Get all PO files
+var listPo = [];
+walk(base_dir, 'po', listPo);
+
+for (var i=0; i < listPo.length; i++) {
+	if (listPo[i] != 'master.pot')
+		poProcessor(listPo[i], stringsDedup);
 }
+
+console.log('* All done.');
 
 
 /**
  * Walk a directory, adding its file to the list.
  * @param dir String The directory to walk.
+ * @param type String The type of files in the directory.
  * @param list Array The list of files to add entries to.
- * @param zip Zip The ZIP object to add each file to. 
- * @param combiner Combiner The Combiner object to add each file to. 
  */
-function walk(dir, list, zip, combiner) {
+function walk(dir, type, list) {
 	var files = fs.readdirSync(dir);
-	for (var i=0; i<files.length; i++) {		
+	for (var i=0; i < files.length; i++) {
 		// Skip hidden files and directories
 		if (files[i].substring(0,1) == ".")
 			continue;
@@ -108,65 +95,63 @@ function walk(dir, list, zip, combiner) {
 		// Skip our index file
 		if (files[i] == index_file)
 			continue;
-		
+
+		var path = dir + '/' + files[i];
+
 		// Check if this is a directory
-		var stat = fs.statSync(dir + '/' + files[i]);
+		var stat = fs.statSync(path);
 		if (stat.isDirectory()) 
 			// Dive in
-			walk(dir + '/' + files[i], list, zip);
+			walk(path, type, list);
 		else {
-			if (list) {
-				var _tmp = dir.replace(base_dir, '') + '/' + files[i];
-				list.push(_tmp.replace(/^\//,''));							
-			}
+
+		// Get file name from path
+			list.push({
+				type: type,
+				path: path,
+				name: path.replace(reJsPath, ''),
+			});
 		}
 	}	
 }
 
 /**
  * Process a file line by line
- * @param filePrefix String The path prefix to prepend when reading the file (will not be shown in POT file); set to '' for none. 
- * @param fileName String The name of the file to process.
- * @param parser String The parser to invoke, either 'html' or 'js'
+ * @param fileItem Object The object with the descritpion of the file to process. 
  * @param stringsRaw Array Where to add the extracted chunks of text to.
  */
 
-function lineProcessor(filePrefix, fileName, parser, stringsRaw) {
+function lineProcessor(fileItem, stringsRaw) {
 	var counter = 0;
 
-	// Create a line-by-line reader and the line processing function
-	var liner = new LineReader(function(line){
+	const data = fs.readFileSync(fileItem.path, 'UTF-8');
+
+    // Split the contents by new line
+    const lines = data.split(/\r?\n/);
+
+	for (var i=0; i < lines.length; i++) {
 		var strings;
 		counter ++;
 
 		// Invoke proper parser
-		if (parser == 'html')
-			strings = stripHtml(line);
-		else if (parser == 'js')
-			strings = stripJs(line);
+		if (fileItem.type == 'html')
+			strings = stripHtml(lines[i]);
+		else if (fileItem.type == 'js')
+			strings = stripJs(lines[i]);
 
 		// Push the strings to the array
 		if (strings) {
-			for (var i=0; i< strings.length; i++) {
+			for (var j=0; j< strings.length; j++) {
 				stringsRaw.push({
-					msgid: strings[i],
+					msgid: strings[j],
 					files: [{
-						file: fileName,
+						file: fileItem.name,
 						line: counter
 					}]
 				});
 			}
 		}
-	});
-
-	// When reading ends, we need to call the sync object - so attach our callback to LineReader's internal signal
-	liner.onSignal('end', function() {
-		// Call the sync object
-		sync.dec();		
-	});
-
-	// Attach the reader to the stream to start reading
-	liner.attachSource(filePrefix + '/' + fileName);
+	}
 }
 
 
@@ -214,8 +199,8 @@ function stripJs(line) {
 		return null;
 
 	// Split by ';' or '+' sign
-    splitted = line.split(reJsSplit);
-    for (var i=0; i<splitted.length; i++) {
+	splitted = line.split(reJsSplit);
+	for (var i=0; i<splitted.length; i++) {
 		// Extract text from single-quotes
 		var stripped = splitted[i].match(reJsStrip1);
 		if (stripped)
@@ -236,7 +221,7 @@ function stripJs(line) {
 
 /**
  * Deduplicate the strings array
- * @param stringsRaw Array Objects, one for each string, with 2 properties: 'msgid' (the text to write) and 'files' (array of objects with properties 'file' - the filename that has the string and 'line' - the line on shich the string occurs).
+ * @param stringsRaw Array Objects, one for each string, with 2 properties: 'msgid' (the text to write) and 'files' (array of objects with properties 'file' - the filename that has the string and 'line' - the line on which the string occurs).
  * @returns Array Similar array as the input, but with duplicate text removed (file and line moved into the 'files' array of each object).
  */
 
@@ -244,9 +229,9 @@ function deduplicate(stringsRaw) {
 	var dedup = [];
 	var match = false;
 	var j = 0;
-	for (var i=0; i<stringsRaw.length; i++) {
+	for (var i=0; i < stringsRaw.length; i++) {
 		match = false;
-		for (j=0; j<dedup.length; j++) {
+		for (j=0; j < dedup.length; j++) {
 			if (stringsRaw[i].msgid == dedup[j].msgid) {
 				match = true;
 				break;
@@ -292,7 +277,7 @@ function generatePot(stringsDedup) {
 		if (stringsDedup[i].comment)
 			files = stringsDedup[i].comment;
 		else {
-			for (var j=0; j<stringsDedup[i].files.length; j++) {
+			for (var j=0; j < stringsDedup[i].files.length; j++) {
 				if (files)
 					files += ', ';
 				files += stringsDedup[i].files[j].file + ':' + stringsDedup[i].files[j].line;
@@ -319,21 +304,25 @@ function generatePot(stringsDedup) {
 
 /**
  * Process a PO file line by line and merge changes into it
- * @param filePrefix String The path prefix to prepend when reading the file (will not be shown in POT file); set to '' for none. 
- * @param fileName String The name of the file to process.
- * @param parser String The parser to invoke, either 'html' or 'js'
- * @param stringsRaw Array Where to add the extracted chunks of text to.
+ * @param fileItem Object The object with the descritpion of the file to process. 
+ * @param stringsDedup Array Deduplicated array of strings (as returned by the deduplicate() function).
  */
 
-function poProcessor(filePrefix, fileName, stringsDedup) {
+function poProcessor(fileItem, stringsDedup) {
 	var status = 'waiting';
 	var text = '';
 	var textMsgid = '';
 	var textComment = '';
 	var strings = [];
 
-	// Create a line-by-line reader and the line processing function
-	var liner = new LineReader(function(line) {
+	const data = fs.readFileSync(fileItem.path, 'UTF-8');
+
+    // Split the contents by new line
+    const lines = data.split(/\r?\n/);
+
+	for (var i=0; i < lines.length; i++) {
+		var line = lines[i];
+
 		// We don't care for lines starting with '#'
 		if (line.indexOf('#') == 0)
 			textComment = line;
@@ -380,81 +369,103 @@ function poProcessor(filePrefix, fileName, stringsDedup) {
 		// If it is any other line, append it to current text
 		else 
 			text += '\n' + line;
-	});
+	}
 
-	// When reading ends, update the PO file
-	liner.onSignal('end', function() {
-		// Flush the last object (because we don't get the emptry lines from the reader)
-		if (status == 'msgstr') {
-			status = 'waiting';
-			strings.push({
-				comment: textComment,
-				msgid: textMsgid,
-				msgstr: text,
+/*
+	// Flush the last object (because we don't get the emptry lines from the reader)
+	if (status == 'msgstr') {
+		status = 'waiting';
+		strings.push({
+			comment: textComment,
+			msgid: textMsgid,
+			msgstr: text,
+		});
+	}		
+*/
+
+	var stringsNew = [];
+
+	// Push all strings that still exist (deleted one will be skipped)
+	for (var i=0; i<strings.length; i++) {
+		for (var j=0; j<stringsDedup.length; j++) {
+			if (strings[i].msgid == stringsDedup[j].msgid) {
+				stringsNew.push(strings[i]);
+				break;
+			}
+		}
+	}
+
+	// Add newly created strings
+	for (var i=0; i<stringsDedup.length; i++) {
+		var found = false;
+		for (var j=0; j<stringsNew.length; j++) {
+			if (stringsNew[j].msgid == stringsDedup[i].msgid) {
+				found = true;
+				break;
+			}
+		}
+
+		if (! found) {
+			stringsNew.push({
+				msgid: stringsDedup[i].msgid,
+				msgstr: '',
+				files: stringsDedup[i].files
 			});
-		}		
-
-		var stringsNew = [];
-
-		// Push all strings that still exist (deleted one will be skipped)
-		for (var i=0; i<strings.length; i++) {
-			for (var j=0; j<stringsDedup.length; j++) {
-				if (strings[i].msgid == stringsDedup[j].msgid) {
-					stringsNew.push(strings[i]);
-					break;
-				}
-			}
 		}
+	}
 
-		// Add newly created strings
-		for (var i=0; i<stringsDedup.length; i++) {
-			var found = false;
-			for (var j=0; j<stringsNew.length; j++) {
-				if (stringsNew[j].msgid == stringsDedup[i].msgid) {
-					found = true;
-					break;
-				}
-			}
-
-			if (! found) {
-				stringsNew.push({
-					msgid: stringsDedup[i].msgid,
-					msgstr: '',
-					files: stringsDedup[i].files
-				});
-			}
-		}
-
-		// Generate the new PO file
-		var po = generatePot(stringsNew);
-		fs.writeFileSync(filePrefix + '/' + fileName, po);
-	});
-
-	// Attach the reader to the stream to start reading
-	liner.attachSource(filePrefix + '/' + fileName);
+	// Generate the new PO file
+	var po = generatePot(stringsNew);
+	fs.writeFileSync(fileItem.path, po);
 
 	// Report
-	console.log("* Updating translations in " + fileName);
+	console.log("* Updating translations in " + fileItem.path);
 }
 
 
 /**
- * Synchronizing counter object
- * Make an instance and pass it a callback, which will be invoked when internal counter reaches 0.
- * @param cb function Callback to be invoked.
+ * Serialisation object
+ * @param array Array The input array, from which one element will be passed to each invocation of the each() function.
+ * @param each function Function to invoke for each element of the input array.
+ * @param done function Function to be invoked after all elements of the input array have been processed.
  */
-function guidoSync(cb) {
-	this.counter = 0;
-	this.inc = function() {
-		this.counter++;
-	};
-	this.dec = function() {
-		this.counter--;
-		if (this.counter == 0) {
-			cb();
-		}
-	};
-}
+function guidoSerialise(array, each, done) {
+    this.counter = -1;
+ 
+    /**
+     * Get current element
+     */
+    this.getEntry = function(id) {
+        if (id) {
+            return array[id];   
+        }
+        else {
+            return array[this.counter]; 
+        }
+    };
+     
+     
+    /**
+     * Process next element of the array or call the done() function  
+     */
+    this.run = function(delay) {
+        this.counter++;
+        var self = this;
+        if (array[this.counter]) {
+            if (typeof(each) === 'function') {
+                setTimeout(function(){
+                    each(array[self.counter]);
+                }, delay);                  
+            }
+        }
+        else {
+            // When last file from the input array is processed...
+            if (typeof(done) === 'function') {
+                done(); 
+            }
+        }
+    };
+};
 
 
 /**
